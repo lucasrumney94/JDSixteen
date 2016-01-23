7cab27234fa29ae2060732c365b44b266bb567d5
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-23 18:52:06 -0500

    Ui and Timer updated

--- JDSixteen/Assets/Scripts/Game Logic Scripts/gameTimer.cs
+++ JDSixteen/Assets/Scripts/Game Logic Scripts/gameTimer.cs
@@ -7,7 +7,7 @@ public class gameTimer : MonoBehaviour
     //All times are in Seconds
     public float startTime = 15.0f;         //This sets the inital timer time
     public float changeStartTimeBy = -5.0f;      //when changeStartTime() is called, this is the default amount it will subtract
-    
+    public float offsetForSlider = 5.0f;
 
     private Slider timerSlider;
     private float actualTime;
@@ -16,12 +16,12 @@ public class gameTimer : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        actualTime = startTime;
-        newStartTime = startTime;
+        actualTime = startTime + offsetForSlider;
+        newStartTime = startTime + offsetForSlider;
 
         timerSlider = GameObject.FindGameObjectWithTag("Timer").GetComponent<Slider>();
-        timerSlider.maxValue = startTime;
-        timerSlider.value = actualTime;
+        timerSlider.maxValue = startTime + offsetForSlider;
+        timerSlider.value = actualTime + offsetForSlider;
     }
 
     // Update is called once per frame
@@ -30,7 +30,7 @@ public class gameTimer : MonoBehaviour
 
         actualTime -= Time.deltaTime;
         timerSlider.value = actualTime;
-        if (actualTime <= 0)
+        if (actualTime <= offsetForSlider)
         {
             //TODO: call method that spawns another line
 
