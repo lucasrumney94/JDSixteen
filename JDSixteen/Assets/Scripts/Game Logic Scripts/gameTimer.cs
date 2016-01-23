cfcdd8abc380129605efcd43488993c0eab845db
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-23 12:37:08 -0500

    Added main scene to Build settings

---
+++ JDSixteen/Assets/Scripts/Game Logic Scripts/gameTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class gameTimer : MonoBehaviour {
+
+    private Slider timerSlider;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timerSlider = GameObject.FindGameObjectWithTag("timer").GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
