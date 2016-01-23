780c09b8c535919c364871f62a300d8bdc741e7e
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-23 17:13:05 -0500

    bomb method started;

---
+++ JDSixteen/Assets/Scripts/Game Logic Scripts/bombMethod.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class bombMethod : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+
+    void explodeBomb(int x, int y, int range)
+    {
+        //take position and increment outwards and destroy objects
+
+        //use gridPoints list for this
+
+        //apply magic effects
+
+    }
+}
