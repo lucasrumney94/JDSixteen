41f08c24c5176078814f7e5da934b0ac2945f4c1
Lucas Rumney <lucasrumney94@gmail.com>
2016-01-23 13:10:06 -0500

    created gameTimer; Updated UI prefab to include GameOver;

--- JDSixteen/Assets/Scripts/Game Logic Scripts/gameTimer.cs
+++ JDSixteen/Assets/Scripts/Game Logic Scripts/gameTimer.cs
@@ -1,20 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class gameTimer : MonoBehaviour {
+public class gameTimer : MonoBehaviour
+{
+    //All times are in Seconds
+    public float startTime = 15.0f;         //This sets the inital timer time
+    public float changeStartTimeBy = -5.0f;      //when changeStartTime() is called, this is the default amount it will subtract
+    
 
     private Slider timerSlider;
+    private float actualTime;
+    private float newStartTime;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-        timerSlider = GameObject.FindGameObjectWithTag("timer").GetComponent<Slider>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        actualTime = startTime;
+        newStartTime = startTime;
+
+        timerSlider = GameObject.FindGameObjectWithTag("Timer").GetComponent<Slider>();
+        timerSlider.maxValue = startTime;
+        timerSlider.value = actualTime;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-	
-	}
+
+        actualTime -= Time.deltaTime;
+        timerSlider.value = actualTime;
+        if (actualTime <= 0)
+        {
+            //TODO: call method that spawns another line
+
+            startTime = newStartTime;
+            actualTime = startTime;
+        }
+    }
+
+    void changeStartTime()
+    {
+        newStartTime += changeStartTimeBy;
+    }
+
 }
--- JDSixteen/Assets/Scripts/UIScripts/buttonMethods.cs
+++ JDSixteen/Assets/Scripts/UIScripts/buttonMethods.cs
@@ -1,23 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class buttonMethods : MonoBehaviour {
+public class buttonMethods : MonoBehaviour
+{
 
     public Canvas pauseMenu;
+    public Canvas youLostMenu;
     public bool paused = false;
+    public bool gameLost = false;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-	
-	}
+        if (gameLost)
+        {
+            Time.timeScale = 0.0f;
+            youLostMenu.enabled = true;
+        }
+    }
 
     public void pause()
     {
@@ -31,7 +38,7 @@ public class buttonMethods : MonoBehaviour {
     }
     public void restart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().ToString(),LoadSceneMode.Single);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
     }
 
 
